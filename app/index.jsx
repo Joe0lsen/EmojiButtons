553ff23cc2faacf0dd4b0d20a6e6e9929aeba707
Joe0lsen <olsen.jcm@gmail.com>
2025-11-07 16:48:19 -0700

    Add main view, remove boiler plate code

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Pressable } from 'react-native'
+import React from 'react'
+
+const app = () => {
+  return (
+    <View>
+      <Pressable>😃</Pressable>
+    </View>
+  )
+}
+
+export default app
